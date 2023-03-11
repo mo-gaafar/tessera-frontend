34343e2b91cb7a9c80bbbbb5967d7464bdc9b7dc
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2023-03-11 23:00:40 +0200

    Created Boilerplate

---
+++ tessera/src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from './components/styles/global';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
