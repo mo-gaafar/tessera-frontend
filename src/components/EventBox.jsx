6dee1248f262beb3e3fb8b54e5590cf81f32c4c3
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2023-03-24 08:50:25 +0200

    added a unit test

--- src/components/EventBox.jsx
+++ src/components/EventBox.jsx
@@ -2,7 +2,7 @@ export default function EventBox(props) {
   return (
     <div className="event__box">
       <img src={props.image} alt="event image" />
-      <h5>{props.eventTitle}</h5>
+      <h5 data-testid="img">{props.eventTitle}</h5>
       <h6>{props.date}</h6>
       <p>{props.description}</p>
       {props.isFree && <p className="free">Free</p>}
