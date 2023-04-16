be6d59a0e386cf2620fef844071237b726a860ff
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2023-04-16 19:45:41 +0200

    created a tierbox compontent

---
+++ src/pages/BookingPopUP/Ticket/TierBox.jsx
@@ -0,0 +1,81 @@
+import {
+  SelectTicket,
+  SelectTickContainer,
+  SelectTickName,
+  IncrementDecrement,
+  SelectTickBottomContainer,
+  BottomContainerHead,
+} from './Ticket.styled';
+export default function TierBox({ element }) {
+  return (
+    <SelectTicket>
+      <SelectTickContainer className="focus">
+        <SelectTickName>{element.tierName}</SelectTickName>
+        <IncrementDecrement>
+          <div
+            className={
+              element.numberOfTicketsSold == element.maxCapacity
+                ? 'incdec'
+                : 'incdecactive'
+            }
+            onClick={() => incrementOrder(index)}
+          >
+            <svg
+              id="plus-chunky_svg__eds-icon--plus-chunky_svg"
+              x="0"
+              y="0"
+              viewBox="0 0 24 24"
+            >
+              <path
+                id="plus-chunky_svg__eds-icon--plus-chunky_base"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M13 11V4h-2v7H4v2h7v7h2v-7h7v-2z"
+              ></path>
+            </svg>
+          </div>
+          <div className="Quantity">{}</div>
+          <div
+            className={
+              element.numberOfTicketsSold == 0 ? 'incdec' : 'incdecactive'
+            }
+            onClick={() => decrementOrder(index)}
+          >
+            <svg
+              id="minus-chunky_svg__eds-icon-minus-chunky"
+              x="0"
+              y="0"
+              viewBox="0 0 24 24"
+            >
+              <g>
+                <path fill="#fff" d="M6.5 11.5h11v1h-11z"></path>
+                <path d="M18 11H6v2h12v-2z"></path>
+              </g>
+            </svg>
+          </div>
+        </IncrementDecrement>
+      </SelectTickContainer>
+      <SelectTickBottomContainer>
+        <BottomContainerHead>
+          <p className="Fee">${element.price}</p>
+          {/* {ticketsAmount[index].discount && (
+      <pre>
+        <p className={classes.price}>
+          {element.price -
+            element.price *
+              ticketsAmount[index].discountpercent -
+            ticketsAmount[index].discountamount}
+          {"  "}
+          <del>{element.price}</del>
+        </p>
+      </pre>
+    )} */}
+          <p className="Sale">
+            Sales end on {element.endSelling}
+            {/* {moment(element.salesStart).format("MMMM Do YYYY")} */}
+          </p>
+        </BottomContainerHead>
+      </SelectTickBottomContainer>
+    </SelectTicket>
+  );
+}
