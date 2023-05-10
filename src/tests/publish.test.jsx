d5c5f26c77ccb1f96bf28e933849c4009d23b06c
CLARA_YOUSSEF <claraashraf2411@gmail.com>
2023-05-10 19:47:26 +0300

    unit testing of publish page

--- src/tests/publish.test.jsx
+++ src/tests/publish.test.jsx
@@ -1,69 +1,43 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import PublishPage from './PublishPage';
+import { render, fireEvent } from '@testing-library/react';
+import PublishPage from '../pages/PublishPage';
 
 describe('PublishPage', () => {
-  it('should render the radio buttons for who can see the event', () => {
-    render(<PublishPage />);
-    const privateRadio = screen.getByLabelText('private');
-    const publicRadio = screen.getByLabelText('public');
-    expect(privateRadio).toBeInTheDocument();
-    expect(publicRadio).toBeInTheDocument();
+  test('renders correctly', () => {
+    const { getByText } = render(<PublishPage />);
+    const headingElement = getByText(/Publish Your Event/i);
+    expect(headingElement).toBeInTheDocument();
   });
 
-  it('should render the password input when private is selected', () => {
-    render(<PublishPage />);
-    const privateRadio = screen.getByLabelText('private');
-    fireEvent.click(privateRadio);
-    const passwordInput = screen.getByLabelText('password');
+  test('shows password input when private audience is selected', () => {
+    const { getByLabelText, getByText } = render(<PublishPage />);
+    const privateRadioButton = getByLabelText('Private');
+    fireEvent.click(privateRadioButton);
+    const passwordInput = getByLabelText('Password');
     expect(passwordInput).toBeInTheDocument();
   });
 
-  it('should not render the link input when private is selected', () => {
-    render(<PublishPage />);
-    const privateRadio = screen.getByLabelText('private');
-    fireEvent.click(privateRadio);
-    const linkInput = screen.queryByLabelText('anyone');
-    expect(linkInput).not.toBeInTheDocument();
+  test('shows start date and time inputs when schedule later is selected', () => {
+    const { getByLabelText, getByText } = render(<PublishPage />);
+    const scheduleLaterRadioButton = getByLabelText('Schedule Later');
+    fireEvent.click(scheduleLaterRadioButton);
+    const startDateInput = getByLabelText('Start Date');
+    const startTimeInput = getByLabelText('Start Time');
+    expect(startDateInput).toBeInTheDocument();
+    expect(startTimeInput).toBeInTheDocument();
   });
 
-  // it('should render the link input when public is selected', () => {
-  //   render(<PublishPage />);
-  //   const publicRadio = screen.getByLabelText('Public');
-  //   fireEvent.click(publicRadio);
-  //   const linkInput = screen.getByLabelText('Link');
-  //   expect(linkInput).toBeInTheDocument();
-  // });
-
-  it('should render the radio buttons for when to publish the event', () => {
-    render(<PublishPage />);
-    const publishNowRadio = screen.getByLabelText('now');
-    const scheduleLaterRadio = screen.getByLabelText('later');
-    expect(publishNowRadio).toBeInTheDocument();
-    expect(scheduleLaterRadio).toBeInTheDocument();
-  });
-
-  it('should not render the schedule input when publish now is selected', () => {
-    render(<PublishPage />);
-    const publishNowRadio = screen.getByLabelText('now');
-    fireEvent.click(publishNowRadio);
-    const scheduleInput = screen.queryByLabelText('later');
-    expect(scheduleInput).not.toBeInTheDocument();
-  });
-
-  // it('should render the schedule input when schedule later is selected', () => {
-  //   render(<PublishPage />);
-  //   const scheduleLaterRadio = screen.getByLabelText('later');
-  //   fireEvent.click(scheduleLaterRadio);
-  //   const scheduleInput = screen.getByLabelText('Schedule');
-  //   expect(scheduleInput).toBeInTheDocument();
-  // });
-
-  it('should call the publish function when publish button is clicked', () => {
-    const publishMock = jest.fn();
-    render(<PublishPage publish={publishMock} />);
-    const publishButton = screen.getByRole('button', { name: 'Publish' });
+  test('submits form data when publish button is clicked', () => {
+    const handleSubmit = jest.fn();
+    const { getByText, getByLabelText } = render(
+      <PublishPage onSubmit={handleSubmit} />
+    );
+    const publicRadioButton = getByLabelText('Public');
+    const publishNowRadioButton = getByLabelText('Publish Now');
+    const publishButton = getByText('Publish');
+    fireEvent.click(publicRadioButton);
+    fireEvent.click(publishNowRadioButton);
     fireEvent.click(publishButton);
-    expect(publishMock).toHaveBeenCalled();
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
   });
 });
